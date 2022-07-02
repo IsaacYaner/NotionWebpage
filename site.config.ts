db2877c81bc4548c8b17731e0d0b1770597895a1
IsaacYaner <47761372+IsaacYaner@users.noreply.github.com>
2022-07-02 13:08:11 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '1a2f2da3e22c4efaa83ff6597f2f4109?v=a9f4f972c0de4d59a13e4c6ac84ef7a8',
+  rootNotionPageId: '533970b19c3742da92841fa8cb869ced',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'BIGD Trial Page',
-  domain: 'https://dusty-cuticle-3ea.notion.site/1a2f2da3e22c4efaa83ff6597f2f4109?v=a9f4f972c0de4d59a13e4c6ac84ef7a8',
+  domain: 'https://dusty-cuticle-3ea.notion.site/TODO-directories-533970b19c3742da92841fa8cb869ced',
   author: 'Isaac', 
 
   // open graph metadata (optional)
